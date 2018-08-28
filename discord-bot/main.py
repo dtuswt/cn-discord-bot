16dac49c77ca32e6acb4d7b3aa03115143b06780
algorythm <anders@wiberg.tech>
2018-08-28 19:52:19 +0200

    Basic bot logic added

--- discord-bot/main.py
+++ discord-bot/main.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
 from pathlib import Path
-import sys
+import sys, signal
 
 discord_client = discord.Client()
 bot_prefix="/"
@@ -25,15 +25,47 @@ async def on_ready():
     print(f"ID: {client.user.id}")
 
     await client.change_presence(game=discord.Game(name="Development in VSCode"))
+    print("Changed status.")
 
-# @client.command(pass_context=True)
-# async def test(ctx):
-#     await client.send("Test!")
-#     # await ctx.send(f"Hello, {ctx.author}. Send sent a message from {ctx.guild}!")
+@client.event
+async def on_command_error(error, ctx):
+    print("ERROR:", error)
 
-@client.command()
-async def foo(ctx, arg):
-    await ctx.send(arg)
+@client.command(pass_context=True)
+async def inside(ctx, *args):
+    if len(args) < 1:
+        await client.say("Wrong use of the commmand. Try `/inside connect` or `/inside connect s123456` instead.")
+
+    cmd = args[0].lower()
+
+    if cmd == "connect":
+        await connect(ctx, args[1:])
+    else:
+        await client.say(f"Unknown command '{cmd}'.'")
+
+async def connect(ctx, args):
+    if len(args) == 1:
+        studyno = get_study_no(args[0])
+        if studyno == None:
+            await client.say(f"The given study number ({args[0]}) is invalid. Try agian.")
+            return
+        await client.say(f"Using study number '{studyno}' and username {ctx.message.author}.'")
+    elif len(args) == 0:
+        await client.say(f"Will connect you to the server with username: {ctx.message.author}.")
+
+def get_study_no(s):
+    if len(s) == 7 and s[1:].isdigit():
+        if s.lower()[0] != 's':
+            return None
+        return s
+    elif len(s) == 6 and s.isdigit():
+        return  f"s{int(s)}"
+    else:
+        return None
+
+def signal_handler(sig, frame):
+    client.close()
+    exit(0)
 
 if __name__ == "__main__":
     print("Using token from 'token.txt'")
